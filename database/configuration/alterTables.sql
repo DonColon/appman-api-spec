4db403606a5a6ff71f0d29e53f04e41fbd997196
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2021-03-24 09:01:21 +0100

    Changed configuration model

--- database/configuration/alterTables.sql
+++ database/configuration/alterTables.sql
@@ -4,11 +4,14 @@ alter table ApplicationConfiguration
     add constraint ApplicationConfiguration_Operator foreign key(operator) references Operator(operatorID);
 
 alter table ViewConfiguration
+	add constraint ViewConfiguration_ApplicationVersion foreign key(app, appVersion) references ApplicationVersion(app, version),
 	add constraint ViewConfiguration_ViewVersion foreign key(`view`, viewVersion) references ViewVersion(`view`, version),
     add constraint ViewConfiguration_ParameterVersion foreign key(parameter, parameterVersion) references ParameterVersion(parameter, version),
     add constraint ViewConfiguration_Operator foreign key(operator) references Operator(operatorID);
     
 alter table ComponentConfiguration
+	add constraint ComponentConfiguration_ApplicationVersion foreign key(app, appVersion) references ApplicationVersion(app, version),
+	add constraint ComponentConfiguration_ViewVersion foreign key(`view`, viewVersion) references ViewVersion(`view`, version),
 	add constraint ComponentConfiguration_ComponentVersion foreign key(`component`, componentVersion) references ComponentVersion(`component`, version),
     add constraint ComponentConfiguration_ParameterVersion foreign key(parameter, parameterVersion) references ParameterVersion(parameter, version),
     add constraint ComponentConfiguration_Operator foreign key(operator) references Operator(operatorID);
--- database/configuration/createTables.sql
+++ database/configuration/createTables.sql
@@ -11,6 +11,8 @@ create table if not exists ApplicationConfiguration(
 );
 
 create table if not exists ViewConfiguration(
+	app int unsigned,
+    appVersion varchar(16),
 	`view` int unsigned,
     viewVersion varchar(16),
     parameter int unsigned,
@@ -19,10 +21,14 @@ create table if not exists ViewConfiguration(
 	operator varchar(3) not null,
     `value` varchar(1024) not null,
     other varchar(1024) not null,
-    primary key(`view`, viewVersion, parameter, parameterVersion, sequence)
+    primary key(app, appVersion, `view`, viewVersion, parameter, parameterVersion, sequence)
 );
 
 create table if not exists ComponentConfiguration(
+	app int unsigned,
+    appVersion varchar(16),
+	`view` int unsigned,
+    viewVersion varchar(16),
 	`component` int unsigned,
     componentVersion varchar(16),
     parameter int unsigned,
@@ -31,5 +37,5 @@ create table if not exists ComponentConfiguration(
 	operator varchar(3) not null,
     `value` varchar(1024) not null,
     other varchar(1024) not null,
-    primary key(`component`, componentVersion, parameter, parameterVersion, sequence)
+    primary key(app, appVersion, `view`, viewVersion, `component`, componentVersion, parameter, parameterVersion, sequence)
 );
